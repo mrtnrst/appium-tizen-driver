9723562bcbc984a4af873f7c584a1063e10d6478
Martin Arista <hello@mrtnrst.com>
2022-06-22 17:40:02 -0400

    Update for fork (#8)
    * update package.json
    
    * remove david dm
    
    * switch to husky and lint-staged
    
    * run eslint and prettier
    
    * switch over authors and version temp
    
    * switch back to single quote
    
    * remove gulp eslint in favor of repo
    
    * first pass at github actions
    
    * second pass at github actions
    
    * workflow cleanup
    
    * switch over badge in readme
    
    * plural test

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
-"use strict";
+'use strict';
 
 const gulp = require('gulp');
 const boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
 boilerplate({
   build: 'appium-tizen-driver',
-  testTimeout: 40000
+  testTimeout: 40000,
+  eslint: false,
 });
--- index.js
+++ index.js
@@ -9,17 +9,16 @@ import * as helpers from './lib/tizen-helpers';
 import * as commands from './lib/commands/index';
 import * as caps from './lib/desired-caps';
 
-
 const { startServer } = server;
 const { TizenDriver } = driver;
 const { helpers: tizenHelpers } = helpers;
 const { commands: tizenCommands } = commands;
 const { commonCapConstraints } = caps;
 
-const DEFAULT_HOST = "localhost";
+const DEFAULT_HOST = 'localhost';
 const DEFAULT_PORT = 4723;
 
-async function main () {
+async function main() {
   let port = yargs.argv.port || DEFAULT_PORT;
   let host = yargs.argv.host || DEFAULT_HOST;
   return await startServer(port, host);
@@ -30,4 +29,10 @@ if (require.main === module) {
 }
 
 export default TizenDriver;
-export { tizenHelpers, tizenCommands, TizenDriver, startServer, commonCapConstraints };
+export {
+  tizenHelpers,
+  tizenCommands,
+  TizenDriver,
+  startServer,
+  commonCapConstraints,
+};
